cdd0aec95d3773c1515b56ac1e5b51c98bb4f860
Rena Hu <r3hu@ucsd.edu>
2022-05-01 17:08:37 -0700

    Fixed error in tester file

--- SkillDemo1Tester.java
+++ SkillDemo1Tester.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemo1Tester {
     @Test
     public void testMultiply() {
-        assertEquals("Output is 4 * 5 = 20", 9, SkillDemo1.multiply(4, 5));
+        assertEquals("Output is 4 * 5 = 20", 20, SkillDemo1.multiply(4, 5));
     }
 }
